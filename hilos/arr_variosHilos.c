c01cc34a2427efe5e52a6bd235090a21f291c098
rosaainz <al2173040136@azc.uam.mx>
2023-09-22 00:45:36 -0600

    Create threads and change name const

--- hilos/arr_variosHilos.c
+++ hilos/arr_variosHilos.c
@@ -3,14 +3,28 @@
 #include<pthread.h>
 #include<time.h>
 #define MAX 100
-#define NUM_HILO 10
+#define NUM_HILOS 10
 
 int ARR[MAX];
 
+void *llenarArreglo(void *arg){
+  int num_hilo = (int)(size_t)arg;
+
+  for( int i=0; i<MAX; i++){
+    ARR[i] = rand() % 1000 + 1;
+  }
+  return 0;
+}
+
 int main(){
 
-  pthread_t h[NUM_HILO];
-  int estado[NUM_HILO];
+  pthread_t h[NUM_HILOS];
+  int estado[NUM_HILOS];
+
+  for(int i=0; i<NUM_HILOS; i++){
+    estado[i] = pthread_create(&h[i], NULL, llenarArreglo, (void*)(size_t)i);
+  }
+
 
   return 0;
 }
