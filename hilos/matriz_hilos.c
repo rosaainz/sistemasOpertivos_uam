0a0e532f4d9c7f1d13942c3748f6f87a78bc63c2
rosaainz <al2173040136@azc.uam.mx>
2023-09-22 22:51:11 -0600

    Add returns on functions (for delete warnings)

--- hilos/matriz_hilos.c
+++ hilos/matriz_hilos.c
@@ -6,14 +6,17 @@
 int A[MAX][MAX];
 
 void *llenarMatriz(){
+  return 0;
 }
 
 void imprimirMatriz(){
+  return 0;
 }
 
 int main(){
   pthread_t h[NUM_H];
   int estado[NUM_H];
 
+  for(
   return 0;
 }
