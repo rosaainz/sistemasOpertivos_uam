1a0cd13775d7f05e703af2ac776c46b9800ad23b
rosaainz <al2173040136@azc.uam.mx>
2023-09-23 00:59:14 -0600

    Create five threads

--- hilos/matriz_estructura.c
+++ hilos/matriz_estructura.c
@@ -19,6 +19,13 @@ int main(){
   pthread_t h[5];
   dato d[5];
 
+  for(int i=0; i<5; i++){
+    d[i].id = i;
+    d[i].valor = 'a' + i;
+
+    pthread_create(&h[i], NULL, funcionHilos, (void*)&d[i]);
+    pthread_join(h[i], NULL);
+  }
 
   return 0;
 }
