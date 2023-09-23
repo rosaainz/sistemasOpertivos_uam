9d7aad2b6b7a2e3c62f288ff25dcc20710ee4fe6
rosaainz <al2173040136@azc.uam.mx>
2023-09-23 00:51:35 -0600

    Declare martix 5x10 and define arrays thread and struct

--- hilos/matriz_estructura.c
+++ hilos/matriz_estructura.c
@@ -6,30 +6,19 @@ typedef struct{
   char valor;
 }dato;
 
+char matriz[5][10];
+
 void *funcionHilos(void *arg){
   dato *d = (dato*) arg;
-  printf("Hilo con el id %d, Valor %c\n", d->id, d->valor);
   pthread_exit(0);
 
   return 0;
 }
 
 int main(){
-  pthread_t h1, h2;
-  dato d1, d2;
+  pthread_t h[5];
+  dato d[5];
 
-  d1.id = 1;
-  d1.valor = 'a';
-
-  d2.id = 2;
-  d2.valor = 'b';
-
-
-  pthread_create(&h1, NULL, funcionHilos, (void*)&d1);
-  pthread_create(&h2, NULL, funcionHilos, (void*)&d2);
-
-  pthread_join(h1, NULL);
-  pthread_join(h2, NULL);
 
   return 0;
 }
