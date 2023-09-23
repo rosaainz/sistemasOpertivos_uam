21c347ad0d7197235638877ff626324399c6d0a2
rosaainz <al2173040136@azc.uam.mx>
2023-09-23 00:21:46 -0600

    Add function pthread_exit() and define values of the structure

--- hilos/estructuras_hilos.c
+++ hilos/estructuras_hilos.c
@@ -9,11 +9,20 @@ typedef struct{
 void *funcionHilos(void *arg){
   dato *d = (dato*) arg;
   printf("Hilo con el id %d, Valor %c\n", d->id, d->valor);
+  pthread_exit(0);
+
   return 0;
 }
 
 int main(){
   pthread_t h[10];
+  dato d1, d2;
+
+  d1.id = 1;
+  d1.valor = 'a';
+
+  d2.id = 2;
+  d2.valor = 'b';
 
 
   return 0;
