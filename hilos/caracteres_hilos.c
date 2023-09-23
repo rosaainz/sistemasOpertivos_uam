e9507feedd406a34400a5a60b390c4c29279accf
rosaainz <al2173040136@azc.uam.mx>
2023-09-23 01:24:58 -0600

    Create threads and def of functions

---
+++ hilos/caracteres_hilos.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include<pthread.h>
+
+void *funcionConstantes(void *arg){
+  char * cadena = (char*) arg;
+  return 0;
+}
+
+void *funcionVocales(void *arg){
+  return 0;
+}
+
+int main(){
+  pthread_t h1, h2;
+  char cadena[50];
+  scanf("%s", cadena);
+
+  pthread_create(&h1, NULL, funcionConstantes, (void*)&cadena);
+  pthread_create(&h2, NULL, funcionVocales, (void*)&cadena);
+  pthread_join(h1, NULL);
+  pthread_join(h2, NULL);
+
+  return 0;
+}
