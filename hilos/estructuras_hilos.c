c43cd1bf8a617a70f0f5785b92bcdd9d421ecb64
rosaainz <al2173040136@azc.uam.mx>
2023-09-23 00:16:34 -0600

    Create function that shows the thread id and its value

--- hilos/estructuras_hilos.c
+++ hilos/estructuras_hilos.c
@@ -6,6 +6,12 @@ typedef struct{
   char valor;
 }dato;
 
+void *funcionHilos(void *arg){
+  dato *d = (dato*) arg;
+  printf("Hilo con el id %d, Valor %c\n", d->id, d->valor);
+  return 0;
+}
+
 int main(){
   pthread_t h[10];
 
