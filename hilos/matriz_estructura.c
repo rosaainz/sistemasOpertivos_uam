53fcc8a5a4ab8b7b36f0f4b3dd22f33e6429cd20
rosaainz <al2173040136@azc.uam.mx>
2023-09-23 01:07:40 -0600

    Fill matrix with a,b...and print

--- hilos/matriz_estructura.c
+++ hilos/matriz_estructura.c
@@ -10,10 +10,25 @@ char matriz[5][10];
 
 void *funcionHilos(void *arg){
   dato *d = (dato*) arg;
+
+  int i = d->id;
+  for(int j=0; j<10; j++){
+    matriz[i][j] = d->valor;
+  }
   pthread_exit(0);
 
   return 0;
 }
+
+void imprimirMatriz(){
+  for(int i=0; i<5; i++){
+    for(int j=0; j<10; j++){
+      printf("%c", matriz[i][j]);
+    }
+    printf("\n");
+  }
+}
+
 
 int main(){
   pthread_t h[5];
@@ -27,5 +42,6 @@ int main(){
     pthread_join(h[i], NULL);
   }
 
+  imprimirMatriz();
   return 0;
 }
