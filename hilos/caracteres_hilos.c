69190233c3be480028fed31e3a58df634eb7e90e
rosaainz <al2173040136@azc.uam.mx>
2023-09-23 19:08:22 -0600

    Count consonants

--- hilos/caracteres_hilos.c
+++ hilos/caracteres_hilos.c
@@ -1,9 +1,24 @@
 #include<stdio.h>
 #include<pthread.h>
+#include<string.h>
 
-void *funcionConstantes(void *arg){
+
+void *funcionConsonantes(void *arg){
   char * cadena = (char*) arg;
   printf("%s\n", cadena);
+  printf("%lu\n", strlen(cadena));
+  int cons = 0;
+
+  for(int i=0; i<strlen(cadena); i++){
+    if(!(cadena[i] == 'a' ||
+           cadena[i] == 'e'||
+            cadena[i] == 'i' ||
+             cadena[i] == 'o' ||
+              cadena[i] == 'u'))
+      cons += 1;
+    }
+      printf("Constantes: %d", cons);
+
   return 0;
 }
 
@@ -18,7 +33,7 @@ int main(){
   char cadena[50];
   scanf("%s", cadena);
 
-  pthread_create(&h1, NULL, funcionConstantes, (void*)&cadena);
+  pthread_create(&h1, NULL, funcionConsonantes, (void*)&cadena);
   pthread_create(&h2, NULL, funcionVocales, (void*)&cadena);
   pthread_join(h1, NULL);
   pthread_join(h2, NULL);
