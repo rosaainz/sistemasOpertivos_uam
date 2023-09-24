dda3655dc06038ac2dbed42a52cbdc585c0bd60b
rosaainz <al2173040136@azc.uam.mx>
2023-09-23 21:57:55 -0600

    Create father process with two threads and son process with two threads

--- procesos_hilos.c
+++ procesos_hilos.c
@@ -1,5 +1,6 @@
 #include<stdio.h>
 #include<pthread.h>
+#include<unistd.h>
 
 void *funcionHiloPadre(void *arg){
   int num_hilo = (int)(size_t)arg;
@@ -19,11 +20,19 @@ int main(){
   int idf;
   idf = fork();
 
-  pthread_create(&h1, NULL, funcionHiloPadre, (void*)(size_t)1);
-  pthread_create(&h2, NULL, funcionHiloHijo, (void*)(size_t)2);
+  if(idf > 0){
+    pthread_create(&h1, NULL, funcionHiloPadre, (void*)(size_t)1);
+    pthread_create(&h2, NULL, funcionHiloPadre, (void*)(size_t)2);
+  }
+  if(idf == 0){
+    pthread_create(&h1, NULL, funcionHiloHijo, (void*)(size_t)1);
+    pthread_create(&h2, NULL, funcionHiloHijo, (void*)(size_t)2);
+  }
 
   pthread_join(h1, NULL);
   pthread_join(h2, NULL);
 
+int wait;
+scanf("%d", wait);
 return 0;
 }
