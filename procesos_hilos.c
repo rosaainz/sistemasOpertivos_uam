3c940bb3c069e53eeeada641744dabca130148b1
rosaainz <al2173040136@azc.uam.mx>
2023-09-23 21:39:16 -0600

    Create threads, functions and process

---
+++ procesos_hilos.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+#include<pthread.h>
+
+void *funcionHiloPadre(void *arg){
+  int num_hilo = (int)(size_t)arg;
+  printf("Proceso padre, hilo  %d\n", num_hilo);
+  return 0;
+}
+
+void *funcionHiloHijo(void *arg){
+  int  num_hilo = (int)(size_t)arg;
+  printf("Proceso hijo, hilo %d\n", num_hilo);
+  return 0;
+}
+
+int main(){
+
+  pthread_t h1, h2;
+  int idf;
+  idf = fork();
+
+  pthread_create(&h1, NULL, funcionHiloPadre, (void*)(size_t)1);
+  pthread_create(&h2, NULL, funcionHiloHijo, (void*)(size_t)2);
+
+  pthread_join(h1, NULL);
+  pthread_join(h2, NULL);
+
+return 0;
+}
