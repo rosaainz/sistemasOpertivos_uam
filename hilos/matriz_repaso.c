2e94bf5be2ced2bd569b7b45c0028246c023bc0f
rosaainz <al2173040136@azc.uam.mx>
2023-09-28 23:23:57 -0600

    Create threads and assign value from console

--- hilos/matriz_repaso.c
+++ hilos/matriz_repaso.c
@@ -5,20 +5,33 @@
 int matriz[N][N];
 
 typedef struct{
-  int id;
+  int renglon;
   char valor;
 }dato;
 
 void * llena_renglon(void*arg){
   dato *d = (dato*)arg;
   for(int i=0; i<N; i++){
-    matriz[d->id][i] = d->valor;
+    matriz[d->renglon][i] = d->valor;
   }
   return NULL;
 }
 
 
 int main(){
+  pthread_t h[N];
+  dato d[N];
+
+  for(int i=0; i<N; i++){
+    printf("Ingresa el valor para el hilo %d: ",i);
+    int valor;
+    scanf("%d", &valor);
+
+    d[i].renglon = 0;
+    d[i].valor = valor;
+
+    pthread_create(&h[i], NULL, llena_renglon, (void*)&d[i]);
+  }
 
   return 0;
 }
