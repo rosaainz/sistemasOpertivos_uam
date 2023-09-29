1b20367cde74b52385075789c1ddd2fc7bb96406
rosaainz <al2173040136@azc.uam.mx>
2023-09-28 21:08:38 -0600

    Add new file matriz_repaso.c and define struct

---
+++ hilos/matriz_repaso.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<pthread.h>
+
+#define N 5
+int matriz[N][N];
+
+typedef struct{
+  int id;
+  char valor;
+}dato;
+
+void * llena_renglon(void*arg){
+  return 0;
+}
+
+
+int main(){
+
+  return 0;
+}
+
