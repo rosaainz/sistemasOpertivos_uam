deda0f6e0fdcaea8dc106f4e6fde38560238fa46
rosaainz <al2173040136@azc.uam.mx>
2023-10-05 13:35:15 -0600

    Define function content and change variable turn by turno

--- hilos/solPeterson.c
+++ hilos/solPeterson.c
@@ -3,10 +3,25 @@
 
 int memComp[2];
 
-int turn = -1;
+int turno = -1;
 int interes[2];
 
 void * intentaRegionCritica(void* args){
+  int id = (int)(size_t)args;
+
+  for(int i=0; i<5; i++){
+    turno = id;
+    interes[id] = 1;
+
+    while(turno ==id && interes[1-id]==1);
+
+    memComp[id] = memComp[id] + 1;
+    memComp[1 - id] = memComp[1 - id] - 1;
+    printf("%d, %d\n", memComp[id], memComp[1 - id]);
+
+    interes[id] = 0;
+  }
+  pthread_exit(0);
   return 0;
 }
 
