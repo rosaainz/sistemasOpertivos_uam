18ca8877aed4c200325bc20f3846cae084d12f10
rosaainz <al2173040136@azc.uam.mx>
2023-10-05 13:18:55 -0600

    Create two threads and define function intentaRegionCritica

--- hilos/solPeterson.c
+++ hilos/solPeterson.c
@@ -1,6 +1,16 @@
 #include<stdio.h>
 #include<pthread.h>
 
+void * intentaRegionCritica(void* args){
+  return 0;
+}
+
 int main(){
+  pthread_t h0, h1;
+  pthread_create(&h0, NULL, intentaRegionCritica, (void*)(size_t)0);
+  pthread_create(&h0, NULL, intentaRegionCritica, (void*)(size_t)1);
+  pthread_join(h0, NULL);
+  pthread_join(h1, NULL);
+
   return 0;
 }
