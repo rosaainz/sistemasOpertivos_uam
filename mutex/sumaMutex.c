4bccaf88d2cd34c2d3d6294976215469d96365ba
rosaainz <al2173040136@azc.uam.mx>
2023-10-07 14:56:42 -0600

    New fie sumaMutex.c

---
+++ mutex/sumaMutex.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+#include<pthread.h>
+
+pthread_mutex_t candado;
+int suma=0;
+
+
+void * intentaRegionCritica(){
+  return 0;
+}
+
+
+void llenarArreglo(){
+}
+
+void imprimirArreglo(){
+}
+
+
+void sumar (){
+}
+
+int main(){
+  return 0;
+}
